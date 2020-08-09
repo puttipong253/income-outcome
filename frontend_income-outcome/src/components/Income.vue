0593f8a69e11b1d1c64fb47900d3206ae8e861c4
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-08-09 23:49:02 +0700

    add style components

---
+++ frontend_income-outcome/src/components/Income.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Income
+    </div>
+</template>
---
+++ frontend_income-outcome/src/views/Income/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <StyledButton>add</StyledButton>
+    </div>
+</template>
+
+<script>
+import {StyledButton} from './index.style.vue';
+
+export default {
+    components: {
+        StyledButton
+    }
+}
+</script>
