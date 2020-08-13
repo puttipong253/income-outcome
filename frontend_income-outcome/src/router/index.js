0f84c8a4dc5f73efc7496d34bb3059b1e0f67eea
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-08-13 10:25:47 +0700

    counter

---
+++ frontend_income-outcome/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Counter from '../views/Counter.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Counter',
+    component: Counter
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ frontend_income-outcome/src/views/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <h1>{{number}}</h1>
+    <button @click="increment">increment</button>
+    <button @click="decrement">decrement</button>
+    <button @click="reset">reset</button>
+    <button @click="setCount">setCount</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { 
+      count: 0,
+      number: 0
+    }
+  },
+  methods: {
+    increment(){
+      this.count++
+    },
+    decrement(){
+      this.count--
+    },
+    reset(){
+      this.count = 0
+    },
+    setCount(){
+      this.number = this.count
+    }
+  },
+}
+</script>
