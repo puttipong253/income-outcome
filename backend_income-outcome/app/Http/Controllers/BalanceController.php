686912c3d7635144d54969227bff5b245fbd56f1
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-08-13 20:44:11 +0700

    create balance api

---
+++ backend_income-outcome/app/Http/Controllers/BalanceController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Balance;
+use Illuminate\Http\Request;
+
+class BalanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Balance::get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $balance = new Balance(['balance' => $request->input('balance')]);
+        $balance->save();
+        return $balance;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Balance::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- backend_income-outcome/routes/api.php
+++ backend_income-outcome/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('incomeOutcome', 'IncomeOutcomeController');
+Route::resource('balance', 'BalanceController');
