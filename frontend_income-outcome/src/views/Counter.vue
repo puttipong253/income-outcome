9155291e06d87cb98b46d701edbfe0a82c46b2ef
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-08-13 20:43:14 +0700

    test api

--- frontend_income-outcome/src/views/Counter.vue
+++ frontend_income-outcome/src/views/Counter.vue
@@ -1,36 +1,59 @@
 <template>
   <div>
-    <h1>{{count}}</h1>
-   
-      <div v-for="number in numbers" :key="number.id"> 
-        {{number.date}}
-        {{number.status}}
-        {{number.count}}
-        {{number.description}}
-      </div>
- 
-    <button @click="increment">increment</button>
-    <button @click="decrement">decrement</button>
-    <button @click="reset">reset</button>
-    <button @click="setCount">setCount</button>
+    <h1 style="color: red">{{count}}</h1>
+    <h1>{{list}}</h1>
+    <h1>sum: {{sum}}</h1>
+
+    <button class="btn btn-warning" @click="increment" style="margin-right: 10px">increment</button>
+    <button class="btn btn-primary" @click="decrement" style="margin-right: 10px">decrement</button>
+    <button class="btn btn-danger" @click="reset" style="margin-right: 10px">reset</button>
+    <button class="btn btn-success" @click="listCount" style="margin-right: 10px">addCount</button>
+    <button class="btn btn-dark" @click="sumCount" style="margin-right: 10px">sum</button>
+    <button class="btn btn-dark" @click="saveCount" style="margin-right: 10px">add</button>
+
+    <br><br>
+    <div class="progress">
+      <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" :style="{width: count+'%'}" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">{{count}}%</div>
+    </div>
+    <div v-for="result in results" :key="result.id"> 
+      {{result.date}}
+      {{result.status}}
+      {{result.count}}
+      {{result.description}}
+    </div>
+
+    <div v-for="balance in balance" :key="balance.balance"> 
+      {{balance.balance}}
+    </div>
   </div>
+  
 </template>
 
 <script>
-import {HTTP} from '../HTTP.js'
+import { HTTP } from '../HTTP.js'
 export default {
   data() {
     return { 
       count: 0,
-      numbers: [],
-
+      setCount: 0,
+      results: [],
+      list:[],
+      sum:0,
+      balance:2000,
     }
   },
+
   mounted(){
     HTTP.get('/incomeOutcome')
     .then(res => (
-      console.log("res",res.data[1].count),
-      this.numbers = res.data
+      console.log("res",res.data),
+      this.results = res.data
+    )),
+
+    HTTP.get('/balance')
+    .then(res => (
+      console.log("resBalance",res.data),
+      this.balance = res.data
     ))
   },
   methods: {
@@ -41,11 +64,29 @@ export default {
       this.count--
     },
     reset(){
-      this.count = 0
+      this.count = 0;
+      this.sum = 0;
+      this.list = [];
     },
-    setCount(){
-      this.number = this.count
+    listCount(){
+      this.list.push(this.count)
+    },
+    sumCount(){
+      this.sum = 0;
+      for (let i = 0; i < this.list.length; i++) {
+          this.sum += this.list[i]
+      }
+    },
+    saveCount(){
+      const myData = {
+        balance:this.sum
+      }
+    
+      HTTP.post(`/balance`,myData)
+          .then(res => (
+              console.log("res",res.data)
+          ))
     }
-  },
+  }
 }
 </script>
